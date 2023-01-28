76341a3e20082fcc1009246acce0ce3254046759
sophia kidawai <sophia.kidwai@telus.com>
2023-01-28 15:27:11 -0800

    implemented user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   
   protect_from_forgery with: :exception
 
   private
+
+
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
@@ -29,4 +30,11 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+    
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    helper_method :current_user
+
 end
